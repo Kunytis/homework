8d03d2421b679d47b15fee78beba31cfb03e18e5
izumru <izumrudodessa@gmail.com>
2024-10-26 13:35:14 -0500

    aaa

---
+++ HOMEWORKNUM1.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self):
+        self.name: "kittyyah"
+        self.price = 340
+
+    def printer(self):
+        print(self.name)
+        print(self.price)
+
+product12 = Product()
+product12.printer()
